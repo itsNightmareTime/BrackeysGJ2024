4da792cef19e8f1cb420d79c82d3f5ec37a6a959
itsBrianTime <superbrian69@gmail.com>
2024-09-11 23:56:11 -0700

    chore:
    added keys that can be used by the scanner.tscn and renamed a folder which led to 79 fucking import file changes.

---
+++ Scripts/key.gd
@@ -0,0 +1,34 @@
+extends Node2D
+
+@onready var key: Sprite2D = $Sprite2D
+var move_speed: float = -10.0
+
+# loads the image and turns it into a texture
+func set_key_type(filename : String) -> void:
+	var image = Image.load_from_file(filename)
+	key.texture = ImageTexture.create_from_image(image)
+
+func remove_key() -> void:
+	self.queue_free()
+
+# makes key fully visible, used for the active key
+func set_alpha_active_key() -> void:
+	var color = key.modulate
+	color.a = 1.0 
+	key.modulate = color
+	
+# adds opacity to the key, used for the keys in and out of queue
+func set_alpha_key_in_queue() -> void:
+	var color = key.modulate
+	color.a = 0.67
+	key.modulate = color
+	
+# adds opacity to the key, used for the keys in and out of queue
+func set_alpha_pressed_key() -> void:
+	var color = key.modulate
+	color.a = 0.50
+	key.modulate = color
+
+# moves key left, handled by scanner
+func move_left() -> void:
+	transform.origin = transform.origin + Vector2(move_speed, 0)

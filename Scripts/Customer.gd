494e635152ae6125cf02988058728ebee402ee14
nightmarefails <harmstrong@armsoft.dev>
2024-09-13 00:24:23 -0400

    chore: spawning enhancments with a signal system

--- Scripts/Customer.gd
+++ Scripts/Customer.gd
@@ -1,15 +1,17 @@
 extends PathFollow2D
 class_name Customer
 
-@export var move_speed: int = 200:
-	set(value):
-		move_speed = value
+signal at_end
 
-func _ready():
-	loop = false
+@export var move_speed: int = 200
+var at_end_of_path: bool = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float):
 	progress = progress + (move_speed * delta)
-	if progress_ratio == 1:
-		free()
+	if progress_ratio == 1 and not at_end_of_path:
+		at_end_of_path = true
+		if not get_parent().is_exit:
+			var signal_emitted = emit_signal("at_end", self)
+		else:
+			free()
--- Scripts/CustomerSpawner.gd
+++ Scripts/CustomerSpawner.gd
@@ -1,21 +1,38 @@
 extends Node2D
 
-@onready var path := $CustomerPath
-
-@export_category("Spawner Config")
-# The max amount of customers to spawn
-@export var num_customers: int
-# Time in seconds between spawns
-@export var spawn_rate: int
+@export_category("Path Config")
+# Designates if the path should spawn a customer
+@export var is_spawner: bool = false
+# Array of Character scenes that can be used
 @export var customer_variants: Array[PackedScene] = []
+# The next path to move through
+@export var next_path: Path2D = null
+# Is this the exit
+@export var is_exit: bool = false
+
+var current_customer = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	for x in customer_variants:
-		var new_customer = x.instantiate()
-		new_customer.move_speed = (randi_range(200, 350))
-		path.add_child(new_customer)
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float):
-	pass
+	if not current_customer and is_spawner:
+		spawn_new_customer()
+
+func spawn_new_customer():
+	var new_customer = customer_variants.pick_random().instantiate()
+	new_customer.move_speed = 250
+	current_customer = new_customer
+	current_customer.at_end.connect(move_customer_to_next_path)
+	add_child(new_customer)
+
+# Move the customer to the next path is specified and reset their progress and stop listening for their at end signal
+func move_customer_to_next_path(node: Node2D):
+	print("received signal at_end")
+	node.reparent(next_path)
+	node.progress = 0
+	node.at_end_of_path = false
+	current_customer.at_end.disconnect(move_customer_to_next_path)
+	current_customer = null
